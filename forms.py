8614165651ac98ebafbe0d4ddf772735b0848389
whoisduley <dulelu01@luther.edu>
2014-11-25 08:23:18 -0600

    wtfcheck

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, SelectField, IntegerField, SubmitField
+from wtforms import StringField, BooleanField, SelectField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 class GeneratorForm(Form):
 	lowerRange = SelectField('lowerRange', choices=[('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
-	upperRange = SelectField('upperRange', choices=[('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
+	upperRange = SelectField('upperRange', choices=[('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')])
 
-	passlength = IntegerField('passlength')
+	passLength = StringField('passLength', [validators.Length(min=25, max=50)], default='25')
 
 	alternating = BooleanField('alternating')
 	double = BooleanField('double')
